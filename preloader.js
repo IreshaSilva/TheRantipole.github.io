9618cfa780f252235fc6978374cf3642e4b6610c
IreshaSilva <wiusilva.2@gmail.com>
2019-10-11 11:59:28 +0530

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,4 +1,4 @@
-$(window).load(function() {
-	$(".loader").delay(2000).fadeOut("slow");
-  $("#overlayer").delay(2000).fadeOut("slow");
-})
+// $(window).load(function() {
+// 	$(".loader").delay(2000).fadeOut("slow");
+//   $("#overlayer").delay(2000).fadeOut("slow");
+// })
